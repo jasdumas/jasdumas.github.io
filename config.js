3a127b45750a71989cf35414acb60e318af001f5
Jasmine Dumas <jasmine.dumas@gmail.com>
2015-02-28 18:55:04 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var dataUrl = 'https://github.com/jasdumas/jasdumas.github.io/blob/master/data/marker-points.csv';
 var maxZoom = 9;
 var fieldSeparator = ',';
-var baseUrl = 'http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpg';
+var baseUrl = 'http://www.openstreetmap.org/#map=9/41.5333/-72.6636';
 var baseAttribution = 'Data, imagery and map information provided by <a href="http://open.mapquest.co.uk" target="_blank">MapQuest</a>, <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>';
 var subdomains = '1234';
 var clusterOptions = {showCoverageOnHover: false, maxClusterRadius: 50};
