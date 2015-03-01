57f02948f7c526a73c4cb45048fc22f1c775dc63
Jasmine Dumas <jasmine.dumas@gmail.com>
2015-02-28 21:36:13 -0500

    Update config.js

--- config.js
+++ config.js
@@ -9,12 +9,12 @@
     //invocation.send(); 
   //}
 //}
-Access-Control-Allow-Origin: http://jasdumas.github.io/
-var dataUrl = 'https://gist.githubusercontent.com/jasdumas/30b4a2e7388be9f894ae/raw/a8afaf2e938b3f6cf1bf70e52736b919520d17d5/marker-points.csv';
+//Access-Control-Allow-Origin: http://jasdumas.github.io/
+var dataUrl = 'https://github.com/jasdumas/jasdumas.github.io/blob/master/data/marker-points.csv';
 //var dataUrl = 'data/marker-points.csv';
 var maxZoom = 15;
 var fieldSeparator = ',';
-Access-Control-Allow-Origin: http://otile{s}.mqcdn.com/
+//Access-Control-Allow-Origin: http://otile{s}.mqcdn.com/
 var baseUrl = 'http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpg';
 var baseAttribution = 'Data, imagery and map information provided by <a href="http://open.mapquest.co.uk" target="_blank">MapQuest</a>, <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>';
 var subdomains = '1234';
