d2a1fdbe450cc4a8fe0b6e47647eaa602b87d500
Jasmine Dumas <jasmine.dumas@gmail.com>
2015-02-28 21:44:03 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,16 @@
+function initialize() {
+  var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+  var mapOptions = {
+    zoom: 4,
+    center: myLatlng
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Hello World!'
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
