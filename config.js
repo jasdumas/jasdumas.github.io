c6bc99895b27d592aeea3dc06cd640656ee2802f
Jasmine Dumas <jasmine.dumas@gmail.com>
2015-02-28 21:35:02 -0500

    Update config.js

--- config.js
+++ config.js
@@ -14,6 +14,7 @@ var dataUrl = 'https://gist.githubusercontent.com/jasdumas/30b4a2e7388be9f894ae/
 //var dataUrl = 'data/marker-points.csv';
 var maxZoom = 15;
 var fieldSeparator = ',';
+Access-Control-Allow-Origin: http://otile{s}.mqcdn.com/
 var baseUrl = 'http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpg';
 var baseAttribution = 'Data, imagery and map information provided by <a href="http://open.mapquest.co.uk" target="_blank">MapQuest</a>, <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>';
 var subdomains = '1234';
