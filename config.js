3dced03ca0f153594320624fda1a554a2268b05f
Jasmine Dumas <jasmine.dumas@gmail.com>
2015-02-28 20:49:13 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var dataUrl = 'data/marker-points.csv';
+var dataUrl = 'http://erichsen-group.com/demoland/datademo/projects/data/sites.csv';
 var maxZoom = 9;
 var fieldSeparator = ',';
 var baseUrl = 'http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpg';
