78b5e3df152007f62d5c112904f94a1c386c6270
Jasmine Dumas <jasmine.dumas@gmail.com>
2015-02-28 22:21:12 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
+Access-Control-Allow-Origin: https://github.com/jasdumas/jasdumas.github.io/
+Access-Control-Allow-Origin: http://otile{s}.mqcdn.com/
 var dataUrl = 'https://github.com/jasdumas/jasdumas.github.io/blob/master/data/marker-points.csv';
 //var dataUrl = 'data/marker-points.csv';
 var maxZoom = 15;
